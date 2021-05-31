71b5a19537c8758d2300f8681a9f297205064ca5
Cloudless67 <cloudlesslakitu67@gmail.com>
2021-05-31 16:24:01 +0900

    Refactoring

---
+++ client/src/constants/index.ts
@@ -0,0 +1,13 @@
+export const itemCartTableHeader = [
+    '코인',
+    '아이템',
+    '가격',
+    '창고\n이동',
+    '캐릭별\n구매',
+    '수량',
+    '구매 캐릭터',
+    '구매\n수량',
+    '계',
+];
+
+export const characterTableHeader = ['캐릭터', '모을 코인', '사용할 코인', '잉여'];
